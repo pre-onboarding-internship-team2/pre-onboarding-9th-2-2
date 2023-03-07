2439af871f3c2542f6f5974c9e45701b5583adf6
Eunseo Choi <yses9296@gmail.com>
2023-03-07 18:22:57 +0900

    initalized with vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <h1>라이크어로컬</h1>
+    </ChakraProvider>
+  );
+}
+
+export default App;
