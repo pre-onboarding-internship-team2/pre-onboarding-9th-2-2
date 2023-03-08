1c9d21e36eeb78721be843ff0be1db10dc8575bf
onezeun <onezeun@gmail.com>
2023-03-08 16:27:55 +0900

    feat: 모달창 구현

--- src/components/product/ProductItem.tsx
+++ src/components/product/ProductItem.tsx
@@ -29,25 +29,25 @@ export default function ProductItem({ product }: ProductProps) {
 
   return (
     <>
-      <Center p={8} onClick={onOpen}>
+      <Center p="8" onClick={onOpen}>
         <Box
-          role={'group'}
-          p={6}
-          maxW={'350px'}
-          minH={'450px'}
-          w={'full'}
+          role="group"
+          p="6"
+          maxW="350px"
+          minH="450px"
+          w="full"
           bg={useColorModeValue('white', 'gray.800')}
-          boxShadow={'2xl'}
-          rounded={'lg'}
-          pos={'relative'}
-          zIndex={1}
+          boxShadow="2xl"
+          rounded="lg"
+          pos="relative"
+          zIndex="1"
         >
           <Box
-            rounded={'lg'}
-            mt={-12}
-            pos={'relative'}
-            maxW={'300px'}
-            height={'300px'}
+            rounded="lg"
+            mt="-12"
+            pos="relative"
+            maxW="300px"
+            height="300px"
             _after={{
               transition: 'all .3s ease',
               content: '""',
@@ -66,33 +66,33 @@ export default function ProductItem({ product }: ProductProps) {
               },
             }}
           >
-            <Image rounded={'lg'} height={300} width={300} objectFit={'cover'} src={IMAGE} />
+            <Image rounded="lg" height="300" width="300" objectFit='cover' src={IMAGE} />
           </Box>
-          <Box pt={5}>
+          <Box pt="5">
             <Flex justifyContent="space-between" alignItems="baseline">
-              <Badge rounded="full" px="2" fontSize="1em">
+              <Badge rounded="full" fontSize="1em" px="4">
                 {product.spaceCategory}
               </Badge>
-              <Text color={'gray.500'} rounded="full" px="2" fontSize="0.8em">
-                상품번호 {product.idx}
+              <Text color='gray.500' rounded="full" px="2" fontSize="0.8em">
+                상품번호 : {product.idx}
               </Text>
             </Flex>
             <Flex mt="3" justifyContent="space-between" alignContent="center">
               <Stack>
-                <Box fontSize="lg" fontWeight="semibold" as="h4" lineHeight="tight" pr={5}>
+                <Box fontSize="lg" fontWeight="semibold" as="h4" lineHeight="tight" pr="5">
                   {product.name}
                 </Box>
-                <Box fontSize="md">{PRICE}원</Box>
+                <Box fontSize="md">{PRICE} 원</Box>
               </Stack>
               <Tooltip
                 label="상품예약"
                 bg="white"
-                placement={'top'}
-                color={'gray.800'}
-                fontSize={'1em'}
+                placement='top'
+                color='gray.800'
+                fontSize='1em'
               >
-                <chakra.a href={'reservations'} display={'flex'}>
-                  <Icon as={FiShoppingCart} h={8} w={8} alignSelf={'center'} />
+                <chakra.a href='reservations' display='flex'>
+                  <Icon as={FiShoppingCart} h="8" w="8" alignSelf='center' />
                 </chakra.a>
               </Tooltip>
             </Flex>
@@ -100,7 +100,7 @@ export default function ProductItem({ product }: ProductProps) {
         </Box>
       </Center>
 
-      <ProductModal isOpen={isOpen} onClose={onClose} product={product}/>
+      <ProductModal isOpen={isOpen} onClose={onClose} product={product} />
     </>
   );
 }
---
+++ src/components/product/ProductModal.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Image,
+  Button,
+  Text,
+  Box,
+  Center,
+  VStack,
+  Flex,
+  Badge,
+} from '@chakra-ui/react';
+import { ProductType } from './PtoductItem.Type';
+
+interface IProps {
+  isOpen: boolean;
+  onClose: () => void;
+  product: ProductType;
+}
+
+export default function ProductModal({ isOpen, onClose, product }: IProps) {
+  const PRICE = product.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return (
+    <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{product.name}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Center mb="3">
+            <Image rounded="lg" h="300" w="300" objectFit="cover" src={product.mainImage} />
+          </Center>
+          <VStack mb="5">
+            <Flex w="100%" justifyContent="space-between" alignItems="baseline" mb="3">
+              <Badge rounded="full" fontSize="1em" px="4" py="1">
+                {product.spaceCategory}
+              </Badge>
+              <Text color='gray.500' rounded="full" fontSize="0.8em">
+                상품번호 : {product.idx}
+              </Text>
+            </Flex>
+            <Flex
+              w="100%"
+              justifyContent="space-between"
+              borderBottom="1px"
+              borderColor="gray.200"
+              p="3"
+            >
+              <Text as="b" color="teal">
+                가격
+              </Text>
+              <Text>{PRICE} 원</Text>
+            </Flex>
+            <Flex
+              w="100%"
+              justifyContent="space-between"
+              borderBottom="1px"
+              borderColor="gray.200"
+              p="3"
+            >
+              <Text as="b" color="teal">
+                1인 최대 구매 가능 갯수
+              </Text>
+              <Text>{product.maximumPurchases}</Text>
+            </Flex>
+            <Box w="100%" justifyContent="space-between" px="3">
+              <Text as="b" color="teal">
+                내용
+              </Text>
+              <Text mt="2">{product.description}</Text>
+            </Box>
+          </VStack>
+        </ModalBody>
+
+        <ModalFooter justifyContent="space-between">
+          <Text color='gray.500'>{product.registrationDate}</Text>
+          <Box>
+            <Button colorScheme="teal" mr="3">
+              예약하기
+            </Button>
+            <Button colorScheme="gray" onClick={onClose}>
+              Close
+            </Button>
+          </Box>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
