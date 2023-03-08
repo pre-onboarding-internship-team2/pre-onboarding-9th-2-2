afc92aee31f00e29c3e20118d19887c961dfc2a1
jiyeon <a1919361@gmail.com>
2023-03-09 05:27:07 +0900

    design : 헤더 sticky 포지션 적용

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,28 @@
 import { useReservations } from "@/contexts/Reservations.context";
 import { Box, Container, Link, Stack } from "@chakra-ui/react";
 import NextLink from "next/link";
+import Filter from "./filter/Filter";
 
 const Header = () => {
   const { reservations } = useReservations();
   return (
-    <Box backgroundColor="red.300">
-      <Container maxW="container.lg" py={2}>
-        <Stack
-          direction="row"
-          justifyContent="flex-end"
-          color="whiteAlpha.900"
-          fontWeight="bold"
-        >
-          <Link as={NextLink} href="/reservations">
-            장바구니 {reservations.length}
-          </Link>
-        </Stack>
+    <Box as="header" position="sticky" top={0} zIndex="sticky">
+      <Box backgroundColor="red.300">
+        <Container maxW="container.lg" py={2}>
+          <Stack
+            direction="row"
+            justifyContent="flex-end"
+            color="whiteAlpha.900"
+            fontWeight="bold"
+          >
+            <Link as={NextLink} href="/reservations">
+              장바구니 {reservations.length}
+            </Link>
+          </Stack>
+        </Container>
+      </Box>
+      <Container maxW="container.lg" py={2} background="chakra-body-bg">
+        <Filter />
       </Container>
     </Box>
   );
