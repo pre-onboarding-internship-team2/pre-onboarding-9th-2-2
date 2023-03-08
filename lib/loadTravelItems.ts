74f98905017d347513cbf81273c7d1008c46c1e4
jiyeon <a1919361@gmail.com>
2023-03-09 02:55:03 +0900

    fix : 더미데이터 api route로 요청하여 받아오는 방식으로 변경
    원격 json 파일 url이 계속 변경되어 nextjs api route 사용

--- lib/loadTravelItems.ts
+++ lib/loadTravelItems.ts
@@ -2,13 +2,11 @@ import { TravelItem } from "@/types/travelItem.type";
 import axios from "axios";
 
 export async function loadTravelItems(): Promise<TravelItem[]> {
-  const url = process.env.DATA_URL;
-  if (!url) return [];
+  const hostUrl = process.env.HOST_URL || "http://localhost:3000";
   try {
-    const response = await axios.get<TravelItem[]>(url);
+    const response = await axios.get<TravelItem[]>(`${hostUrl}/api/mock-data`);
     return response.data;
   } catch (error) {
-    console.error(error);
-    return [];
+    throw error;
   }
 }
